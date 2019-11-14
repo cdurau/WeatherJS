9ff59dad6fccc29a48f3d33def1e74eed10cdaa6
cdurau <stylano1@gmail.com>
2019-11-14 01:54:58 +0100

    Updated API URL

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,24 @@
 class Weather {
-  // Sample URL: http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=7aa218de17e06690d7f26297b6d97f41
   constructor(city, country, units, lang) {
-    this.appId = '7aa218de17e06690d7f26297b6d97f41';
-    this.city = city;
-    this.country = country;
-    this.units = units;
-    this.lang = lang;
+    this.appId = '7aa218de17e06690d7f26297b6d97f41'
+    this.city = city
+    this.country = country
+    this.units = units
+    this.lang = lang
   }
 
   // Fetch weather from API
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&units=${this.units}&lang=${this.lang}&appid=${this.appId}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&units=${this.units}&lang=${this.lang}&appid=${this.appId}`)
 
-    const responseData = await response.json();
+    const responseData = await response.json()
 
-    return responseData;
+    return responseData
   }
 
   // Change Weather Location
   changeLocation(city, country) {
-    this.city = city;
-    this.country = country;
+    this.city = city
+    this.country = country
   }
 }
